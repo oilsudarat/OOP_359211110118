cf0a6b3732b8f9f64cbc314d816fa03bb23abad8
oilsudarat <50000mobile7631@gmail.com>
2017-11-17 10:31:50 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110118/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110118.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World I'm OIL");
+    }//main
+}//class
