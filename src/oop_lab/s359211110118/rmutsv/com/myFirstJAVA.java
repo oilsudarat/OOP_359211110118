b42de6ab5e237859821f55fce3c0236adffb6663
oilsudarat <50000mobile7631@gmail.com>
2017-11-17 11:14:19 +0700

    Successfuly information data

--- src/oop_lab/s359211110118/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110118/rmutsv/com/myFirstJAVA.java
@@ -1,8 +1,12 @@
 package oop_lab.s359211110118.rmutsv.com;
 
+import java.util.concurrent.SynchronousQueue;
+
 public class myFirstJAVA {
 
     public static void main(String[] args) {
         System.out.println("Hello World I'm OIL");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("RMUTSV");
     }//main
 }//class
